4d3b855137c88e3934f62de8480e4bdc16a95a0f
Mohamad amgad elsaid <65943047+MavicaSoftwareDev@users.noreply.github.com>
2022-08-01 23:10:50 +0200

    project for arabic ready ##

--- printing_app/lib/services/printing/printing.dart
+++ printing_app/lib/services/printing/printing.dart
@@ -1,20 +1,28 @@
-import 'dart:html';
-
+import 'package:flutter/cupertino.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:printing/printing.dart';
 
 class Printer {
   final doc = pw.Document();
-  void addPageToDoc() {
+  void addPageToDoc() async {
+    final font = await fontFromAssetBundle(
+        'assets/fonts/NotoSansArabic-VariableFont_wdth,wght.ttf');
     doc.addPage(
       pw.Page(
         pageFormat: PdfPageFormat.a4,
         build: (pw.Context context) {
           return pw.Center(
-            child: pw.Text('My First Printed PDF',
-                style: pw.TextStyle(fontSize: 48)),
-          ); // Center
+              child: pw.Directionality(
+            textDirection: pw.TextDirection.rtl,
+            child: pw.Text(
+              "السلام عليكم",
+              style: pw.TextStyle(
+                font: font,
+                fontSize: 48.0,
+              ),
+            ),
+          )); // Center
         },
       ),
     );
